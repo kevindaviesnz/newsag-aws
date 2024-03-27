36607b25d527b9e12e4751f241a4c442a9e6ec13
Kevin Davies <kevindavies@Kevins-MacBook-Pro.local>
2024-03-27 16:57:33 +1300

    expired news

---
+++ lambda/dynamodb/remove_expired_news/lambda_function.py
@@ -0,0 +1,8 @@
+
+def remove_expired_news():
+    #"doc-example-table-movies", "moviedata.json", boto3.resource("dynamodb")
+    db_resource = boto3.resource("dynamodb")
+    pass
+
+def lambda_handler(event: list, context: list) -> list:
+    pass
---
+++ lambda/dynamodb/remove_expired_news/test_handler.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture
+def test_data():
+    return {
+
+    }
+    
+def test_remove_expired_news():
+    pass
+    
+def test_lambda_handler():
+    test_remove_expired_news()    
+
+    pass
