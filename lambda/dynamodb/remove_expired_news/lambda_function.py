efe5cfb422a4d9e73de05c37a35622e0a4491319
Kevin Davies <kevindavies@Kevins-MacBook-Pro.local>
2024-03-27 17:56:00 +1300

    remove expired news

--- lambda/dynamodb/remove_expired_news/lambda_function.py
+++ lambda/dynamodb/remove_expired_news/lambda_function.py
@@ -1,8 +1,30 @@
+# @see https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/python/example_code/dynamodb/GettingStarted/scenario_getting_started_movies.py#L322
+
+import boto3
+from botocore.exceptions import ClientError
 
 def remove_expired_news():
+    
     #"doc-example-table-movies", "moviedata.json", boto3.resource("dynamodb")
-    db_resource = boto3.resource("dynamodb")
-    pass
+    dyn_resource = boto3.resource("dynamodb")
+    table = dyn_resource.Table('NewsArticles')
+    table.load()
+    
+    try:
+        
+        response = table.scan(
+            FilterExpression=boto3.dynamodb.conditions.Attr('ttl').eq('86400')
+        )
+        
+        items = response['Items']
+        for item in items:
+            table.delete_item(Key={'uuid': item['uuid']})
+            pass
+        
+    except ClientError as err:
+        raise
+    
+
 
 def lambda_handler(event: list, context: list) -> list:
     pass
--- lambda/dynamodb/remove_expired_news/test_handler.py
+++ lambda/dynamodb/remove_expired_news/test_handler.py
@@ -1,5 +1,6 @@
 import pytest
 
+from lambda_function import remove_expired_news
 
 @pytest.fixture
 def test_data():
@@ -8,6 +9,7 @@ def test_data():
     }
     
 def test_remove_expired_news():
+    remove_expired_news()
     pass
     
 def test_lambda_handler():
