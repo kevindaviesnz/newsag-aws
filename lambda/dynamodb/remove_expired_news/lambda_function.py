12196230f8bfaabb7877ea45f66ec8260f6438af
kevin davies <kevin@debian-desktop.debian-desktop>
2024-03-28 16:43:46 +1300

    ...

--- lambda/dynamodb/remove_expired_news/lambda_function.py
+++ lambda/dynamodb/remove_expired_news/lambda_function.py
@@ -21,23 +21,26 @@ def remove_expired_news():
         
         # Get current timestamp in seconds
         current_timestamp = int(datetime.datetime.now().timestamp())
-        
-        response = table.scan(
-            FilterExpression=boto3.dynamodb.conditions.Attr('ttl').lt(current_timestamp - boto3.dynamodb.conditions.Attr('ts') )
-        ) 
+
+        response = table.scan()        
 
         items = response['Items']
         
         for item in items:
             # @see https://www.bitslovers.com/crud-with-python-and-dynamodb/
             # @see https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb/table/delete_item.html
-            table.delete_item(
-                Key={
-                    'uuid': item['uuid'],
-                    'category': item['category']
-                }
+            ttl = item.get('ttl')
+            ts = item.get('ts')
+            expiration_time_stamp = ts + ttl
+            if expiration_time_stamp < current_timestamp   :
+                table.delete_item(
+                    Key={
+                        'uuid': item['uuid'],
+                        'category': item['category']
+                    }
         )
-        
+                
+
     except ValidationError as err:
         
         logger.error(
@@ -60,9 +63,13 @@ def remove_expired_news():
 
 
 def lambda_handler(event: list, context: list) -> list:
-    event = {}
+
     remove_expired_news()
-    pass
+    return {
+        'statusCode': 200,
+        'container_tag': event['container_tag'],
+        'news_site_url': event['news_site_url']
+    }
 
 if __name__ == "__main__":
     remove_expired_news()
