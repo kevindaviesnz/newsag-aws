da3153668fc1ca06897442be7e82b7bce414d43d
Kevin Davies <kevindavies@Kevins-MacBook-Pro.local>
2024-04-01 11:17:10 +1300

    ...

--- lambda/parse_news_site_into_S3/lambda_function.py
+++ lambda/parse_news_site_into_S3/lambda_function.py
@@ -10,107 +10,82 @@ from bs4 import BeautifulSoup
 
 import logging
 
+# @todo logging
 logger = logging.getLogger(__name__)
-
 
 s3_client = boto3.client('s3')
 
 def lambda_handler(event: list, context: list) -> list:
     
-    try: 
+    # @todo add try catch
+    news_site_url = event['news_site_url']
+                            
+    news_site_html = fetch_html_from_news_site(news_site_url)
+    
+    # Returns an array of articles
+    article_blocks = parse_html_into_article_blocks(news_site_html=news_site_html, article_block_tag=event['article_block_tag'])
+    
+    # Convert containers into json and convert into bytes
+    article_blocks_json_data_bytes = json.dumps(article_blocks).encode('utf-8')
 
-        news_site_url = event['news_site_url']
-                              
-        news_site_html = fetch_html_from_news_site(news_site_url, event['container_tag'])
-        containers = parse_html_into_containers(news_site_html, event['container_tag'])
+    # Generate a unique S3 key for the articles
+    s3_key = f'kdaviesnz.{news_site_url.replace("//", "_").replace(":", "_")}.json'
 
-        print('containers')        
-        print(containers)
-        # Convert containers into json and convert into bytes
-        containers_json_data_bytes = json.dumps(containers).encode('utf-8')
-        
-        # Generate a unique S3 key for the articles
-        s3_key = f'kdaviesnz.{news_site_url.replace("//", "_").replace(":", "_")}.json'
+    # Store article bloocks in a bucket as json
+    bucket = "kdaviesnz-news-bucket"
+    s3_client.put_object(Body=article_blocks_json_data_bytes, Bucket=bucket, Key=s3_key)
 
-        # Store articles in a bucket
-        bucket = "kdaviesnz-news-bucket"
-        s3_client.put_object(Body=containers_json_data_bytes, Bucket=bucket, Key=s3_key)
-
-        # Generate a presigned URL for the S3 object
-        containers_json_url = s3_client.generate_presigned_url(
-            'get_object',
-            Params={'Bucket': bucket, 'Key': s3_key},
-            ExpiresIn=432000   # URL expiration time (e.g., 5 days)
-        )
-
-    except Exception as err:
-
-        logger.error(
-            "Error when parsing news site. %s: %s", 
-            err.response["Error"]["Code"], 
-            err.response["Error"]["Message"]
-        )
-
-        return {
-            'statusCode':500,
-            'error': err
-        }
+    # Generate a presigned URL for the S3 object
+    article_blocks_json_url = s3_client.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': bucket, 'Key': s3_key},
+        ExpiresIn=432000   # URL expiration time (e.g., 5 days)
+    )
     
     # Articles JSON url: https://kdaviesnz-news-bucket.s3.amazonaws.com/kdaviesnz.https__kdaviesnz-news-bucket.s3.amazonaws.com/kdaviesnz.https__foxnews.com.json%3FAWSAccessKeyId%3DAKIA42RD47OJM3V6Q2HU%26Signature%3DVjtNNUSaPPVJG0XyxygUIMxnJQU%253D%26Expires%3D1711854291.json?AWSAccessKeyId=AKIA42RD47OJM3V6Q2HU&Signature=t6uNdP5KV1zFHvPWe8q8P8zODyM%3D&Expires=1711854427
     return {
         'statusCode': 200,
-        'containers_json_url': containers_json_url,
-        'container_tag': event['container_tag'],
+        'article_blocks_json_url': article_blocks_json_url,
+        'article_block_tag': event['article_block_tag'],
         'news_site_url': event['news_site_url']
     }
+        
+        
+def fetch_html_from_news_site(news_site_url:str) -> str:
     
-def extract_tag_chunks(text:str, tag: str) -> list:
-    pattern = r"<" + tag + r"(?:\s+[^>]*?)?>.*?</" + "div" + r">"
-    chunks = re.findall(pattern, text, re.DOTALL)
-    return chunks
-
-def fetch_html_from_news_site(news_site_url:str, container_tag: str) -> str:
-    
+    # @todo do we need a try catch here?
     response = requests.get(news_site_url)
     response.raise_for_status()  # Raise an exception for bad status codes
-    html_content = response.text
+    return response.text
 
-    containers_html = extract_tag_chunks(html_content, container_tag)
-    
-    # Convert elements_data to JSON string
-    return json.dumps(containers_html)
 
-def parse_html_into_containers(news_site_html: str, container_tag: str) ->list:
-
-    print('parsed html')
-    print(news_site_html)
-    
-    print('container tag:')
-    print(container_tag)
+def parse_html_into_article_blocks(news_site_html: str, article_block_tag: str, **kwargs) -> list:
     soup = BeautifulSoup(news_site_html, 'html.parser')
-    container_tag = "<" + container_tag + ">"
-    print('container tag:')
-    print(container_tag)
-    container_tag = '<div class=\"c-NewsTile-item\">'
-
-    # Extract the containers html directly into an array
-    return [element.prettify() for element in soup.find_all(container_tag)]
-
-
-
+    
+    # Extract tag name and class attributes from article_block_tag
+    match = re.match(r'<([^>\s]+)', article_block_tag)
+    if match:
+        tag_name = match.group(1)
+    else:
+        raise ValueError("Invalid article_block_tag format")
+    
+    class_attr = re.findall(r'class="([^"]*)"', article_block_tag)  # Extract class attribute
+    
+    # Prepare keyword arguments for find_all function
+    find_all_kwargs = {}
+    if class_attr:
+        find_all_kwargs['class_'] = class_attr
+    
+    # Extract the news site html article blocks into an array
+    article_blocks = [str(element) for element in soup.find_all(name=tag_name, **find_all_kwargs)]
+    return article_blocks
+    
 
 if __name__ == "__main__":
     
-    '''
-    event = {
-        "news_site_url":"https://foxnews.com",
-        "container_tag":'div class="m"'
-    }
-    '''
-
     event = {
         "news_site_url":"https://www.newshub.co.nz/home.html",
-        "container_tag": 'div class="c-NewsTile"'
+        "article_block_tag": '<div class="c-NewsTile"-item>'
     }
     
     result = lambda_handler(event=event, context=None)
