bedaf77576eeca42e1b976e6e78f6bf772f64e01
Kevin Davies <kevindavies@Kevins-MacBook-Pro.local>
2024-04-02 12:44:52 +1300

    ...

--- lambda/foxnews/lambda_function.py
+++ lambda/foxnews/lambda_function.py
@@ -1,61 +1,48 @@
 # This file: lambda/foxnews/lambda_function.py
+# To install a library in the current directory:
+# pip3 install requests --target .
+# zip -r <output_zip_file> <directory_to_zip>
 from bs4 import BeautifulSoup
 import json
-import requests
-import boto3
 import datetime
 import hashlib
-
 import logging
 
 logger = logging.getLogger(__name__)
 
-s3_client = boto3.client('s3')
-
 def lambda_handler(event, context):
-    # json items are contained in the `Items` array
     try:
-        response = requests.get(event["article_blocks_json_url"])
-        response.raise_for_status()  # Raise an exception for HTTP error responses
-        articles_json = response.json()  # This method parses the JSON response into a Python dict or list
-        event["Items"] = articles_json
+        raw_articles_data = event['body']
         articles = []
-        for item in event['Items']:
+        for item in raw_articles_data:
             item_parsed = foxnews_parse_article_content(item)
             if (item_parsed != None):
                 articles.append(item_parsed)
+                
         top_articles = articles[:50]
         
         return {
             "statusCode": 200,
-            "body": json.dumps(top_articles)
+            "body": json.dumps(top_articles),
+            "article_block_tag": event["article_block_tag"],
+            "news_site_url": event["news_site_url"]
         }
     
     except Exception as err:
-
-        logger.error(
-            "Error when parsing news data into json. %s: %s", 
-            err.response["Error"]["Code"], 
-            err.response["Error"]["Message"]
-        )
-
+        logger.error("Error when parsing news data into json. %s", err)
         return {
             'statusCode':500,
-            'error': err
+            'error': str(err)
         }
-
 
 def foxnews_parse_article_content(article_element: str):
     soup = BeautifulSoup(article_element, 'html.parser')
     
-    # Find the heading tag and a tag within it
     heading_tag = soup.find('header')
     a_tag = heading_tag.find('a', {'data-omtr-intcmp': True}) if heading_tag else None
     
     if a_tag:
         uri = a_tag.get('href')
-
-        # Get current timestamp in seconds
         current_timestamp = int(datetime.datetime.now().timestamp())
 
         article_container = {
@@ -67,27 +54,29 @@ def foxnews_parse_article_content(article_element: str):
             'ts': current_timestamp
         }
 
-        # Find picture and img tags
         img_tag = soup.find('img')
         if img_tag:
             src = f"http:{img_tag.get('src')}"
             article_container["images"] = [src] 
 
-        # Add a hash as id
-        article_container["id"] = hashlib.sha256(json.dumps(article_container, sort_keys=True).encode())
+        hash_object = hashlib.sha256(json.dumps(article_container, sort_keys=True).encode())
+        article_container["id"] = hash_object.hexdigest()
         
         return article_container
     
     else:
-
         return None
 
 if __name__ == "__main__":
+    
     event = {
         "statusCode": 200, 
         "article_blocks_json_url": "https://kdaviesnz-news-bucket.s3.amazonaws.com/kdaviesnz.https__www.foxnews.com.json?AWSAccessKeyId=AKIA42RD47OJIMOJB6N5&Signature=hEYP2okJhUrIV9VkyxkmTt9I2L8%3D&Expires=1712364087",
         "article_block_tag": "<article class=\"article\">", 
-        "news_site_url": "https://www.foxnews.com"
+        "news_site_url": "https://www.foxnews.com",
+        "body": ["<article class=\"article story-1\">\n<div class=\"m\">\n<a data-adv=\"hp1bt\" data-omtr-intcmp=\"fnhpbt1\" href=\"https://www.foxnews.com/politics/biden-says-he-didnt-do-that-when-asked-about-proclaiming-easter-as-trans-day-of-visibility\">\n<picture>\n<source media=\"(max-width: 767px)\" srcset=\"//a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2024/03/343/193/Biden-Easter-bunny.jpg?tl=1&amp;ve=1, //a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2024/03/686/386/Biden-Easter-bunny.jpg?tl=1&amp;ve=1 2x\"/>\n<source media=\"(min-width: 768px) and (max-width: 1023px)\" srcset=\"//a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2024/03/720/405/Biden-Easter-bunny.jpg?tl=1&amp;ve=1, //a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2024/03/1440/810/Biden-Easter-bunny.jpg?tl=1&amp;ve=1 2x\"/>\n<source media=\"(min-width: 1024px) and (max-width: 1279px)\" srcset=\"//a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2024/03/540/304/Biden-Easter-bunny.jpg?tl=1&amp;ve=1, //a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2024/03/1080/608/Biden-Easter-bunny.jpg?tl=1&amp;ve=1 2x\"/>\n<source media=\"(min-width: 1280px) and (max-width: 1439px)\" srcset=\"//a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2024/03/520/293/Biden-Easter-bunny.jpg?tl=1&amp;ve=1, //a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2024/03/1040/586/Biden-Easter-bunny.jpg?tl=1&amp;ve=1 2x\"/>\n<source media=\"(min-width: 1440px)\" srcset=\"//a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2024/03/627/353/Biden-Easter-bunny.jpg?tl=1&amp;ve=1, //a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2024/03/1254/706/Biden-Easter-bunny.jpg?tl=1&amp;ve=1 2x\"/>\n<img alt=\"President Biden responds to backlash about proclaiming Easter as 'Trans Day of Visibility'\" height=\"405\" src=\"//a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2024/03/720/405/Biden-Easter-bunny.jpg?tl=1&amp;ve=1\" width=\"720\"/>\n</picture>\n<div class=\"kicker default\"><span class=\"kicker-text\">'I DIDN'T DO THAT'</span></div>\n</a>\n</div>\n<div class=\"info\">\n<header class=\"info-header\">\n<h3 class=\"title\">\n<a data-adv=\"hp1bt\" data-omtr-intcmp=\"fnhpbt1\" href=\"https://www.foxnews.com/politics/biden-says-he-didnt-do-that-when-asked-about-proclaiming-easter-as-trans-day-of-visibility\">President Biden responds to backlash about proclaiming Easter as 'Trans Day of Visibility'</a>\n</h3>\n</header>\n<div class=\"content\">\n</div>\n</div>\n</article>"]
     }
+
     parsed_articles = lambda_handler(event=event, context=None)
     print(parsed_articles)
+
--- lambda/postscraping/lambda_function.py
+++ lambda/postscraping/lambda_function.py
@@ -1,53 +1,44 @@
 import logging
-import json
 import boto3
-
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError, ValidationError
 
 logger = logging.getLogger(__name__)
 
 # Initialize DynamoDB client
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('your_table_name') 
+table = dynamodb.Table('NewsArticles') 
 
 def lambda_handler(event, context):
     
     try:
-        
-        articles = json.loads(event['body'])
-        
-        # Filter out articles that already exist in DynamoDB
+        articles = event['body'] 
         filtered_articles = list(filter(filter_existing_articles, articles))
-
-        # We will be passing the articles directly to DynamoDB via state machine
         return filtered_articles
+  
+    except ValidationError as err:
         
-        pass
-
-    except Exception as err:
-
         logger.error(
-            "Error when parsing news data into json. %s: %s", 
+            "Validation error. %s: %s", 
             err.response["Error"]["Code"], 
             err.response["Error"]["Message"]
         )
 
-        return {
-            'statusCode':500,
-            'error': err
-        }
-      
-        
-def filter_existing_articles(article):
-    
-    # Check if a matching record exists in DynamoDB
-    # "id" is the partition key and is a hash of the article.
-    response = table.get_item(
-        Key={
-            'id': article["id"]  
-        }
-    )
+        raise
 
-    # If a matching record exists in DynamoDB, filter it out
+    except ClientError as err:
+        
+        logger.error(
+            "Client error!!!. %s: %s", 
+            err.response["Error"]["Code"], 
+            err.response["Error"]["Message"]
+        )
+
+        raise
+          
+
+def filter_existing_articles(article):
+    response = table.get_item(Key={'id': article["id"], 'category': article['category']})
     if 'Item' in response:
         return False
     else:
@@ -55,11 +46,43 @@ def filter_existing_articles(article):
         
         
 if __name__ == "__main__":
+    sample_articles = [
+        {
+            "id": "abc-123",
+            "uri": "https://example.com/article1",
+            "headline": "Sample Headline 1",
+            "category": "news",
+            "images": ["image1.jpg", "image2.jpg"],
+            "ttl": 86400,
+            "ts": "2024-04-02T12:00:00Z"
+        },
+        {
+            "id": "abc-124",
+            "uri": "https://example.com/article2",
+            "headline": "Sample Headline 2",
+            "category": "sports",
+            "images": ["image3.jpg", "image4.jpg"],
+            "ttl": 86400,
+            "ts": "2024-04-02T13:00:00Z"
+        },
+        {
+            "id": "abc-125",
+            "uri": "https://example.com/article3",
+            "headline": "Sample Headline 3",
+            "category": "world",
+            "images": [],
+            "ttl": 86400,
+            "ts": "2024-04-02T14:00:00Z"
+        }
+    ]
+
     event = {
         "statusCode": 200, 
         "article_blocks_json_url": "https://kdaviesnz-news-bucket.s3.amazonaws.com/kdaviesnz.https__www.foxnews.com.json?AWSAccessKeyId=AKIA42RD47OJIMOJB6N5&Signature=hEYP2okJhUrIV9VkyxkmTt9I2L8%3D&Expires=1712364087",
         "article_block_tag": "<article class=\"article\">", 
-        "news_site_url": "https://www.foxnews.com"
+        "news_site_url": "https://www.foxnews.com",
+        "body": sample_articles
     }
+    
     parsed_articles = lambda_handler(event=event, context=None)
     print(parsed_articles)
